6095d39730eeb2b1abeb648eb5c076cdb21877a5
andrelopesmds <andrelopesmds@gmail.com>
2021-09-20 21:16:25 -0300

    fix: encode uri

--- src/lambdas/message-sender.ts
+++ src/lambdas/message-sender.ts
@@ -2,15 +2,13 @@ import { constants } from '../constants'
 import { getRequest } from '../helper'
 
 exports.handler = async (event: any) => {
-  console.log(`${new Date()} - ${JSON.stringify(event)}`)
+  console.log(`Event: ${JSON.stringify(event)}`)
 
   const message = event.Records[0].Sns.Message;
 
-  console.log(message)
+  const encodedURI = encodeURI('https://api.telegram.org/bot' + constants.BOT_TOKEN + '/sendMessage?chat_id=' + constants.BOT_CHAT_ID + '&parse_mode=Markdown&text=' + message)
 
-  await getRequest('https://api.telegram.org/bot' + constants.BOT_TOKEN + '/sendMessage?chat_id=' + constants.BOT_CHAT_ID + '&parse_mode=Markdown&text=' + message)
+  await getRequest(encodedURI)
 
-  return {
-    statusCode: 200, body: JSON.stringify(event)
-  }
+  console.log(`Message sent: ${message}`)
 };
