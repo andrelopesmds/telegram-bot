8d439a6c12f2894994188e2f1678fbb1382d06ba
andrelopesmds <andrelopesmds@gmail.com>
2021-09-23 22:43:37 -0300

    add unit tests for infrastructure

---
+++ test/telegram-bot-stack.test.ts
@@ -0,0 +1,86 @@
+import { TelegramBotStack } from "../lib/telegram-bot-stack";
+import { App } from "monocdk";
+import { haveResourceLike, countResources, expect as expectCdk, countResourcesLike } from '@monocdk-experiment/assert'
+
+describe('cdk tests', () => {
+  let stack: TelegramBotStack;
+
+  beforeAll(() => {
+    const app = new App()
+    stack = new TelegramBotStack(app, 'stack-id')
+  })
+
+  test('should create a topic', () => {
+    expectCdk(stack).to(countResources('AWS::SNS::Topic', 1))
+
+    expectCdk(stack).to(haveResourceLike('AWS::SNS::Topic', {
+      TopicName: 'telegram-bot-topic'
+    }))
+  })
+
+  test('should create lambdas', () => {
+    expectCdk(stack).to(countResources('AWS::Lambda::Function', 3))
+
+    expectCdk(stack).to(haveResourceLike('AWS::Lambda::Function', {
+      FunctionName: 'telegram-bot-stocks-tracker',
+      Runtime: 'nodejs14.x'
+    }))
+
+    expectCdk(stack).to(haveResourceLike('AWS::Lambda::Function', {
+      FunctionName: 'telegram-bot-crypto-tracker',
+      Runtime: 'nodejs14.x'
+    }))
+
+    expectCdk(stack).to(haveResourceLike('AWS::Lambda::Function', {
+      FunctionName: 'telegram-bot-message-sender',
+      Runtime: 'nodejs14.x'
+    }))
+  })
+
+  test('should create permissions to invoke lambdas', () => {
+    expectCdk(stack).to(countResources('AWS::Lambda::Permission', 3))
+
+    expectCdk(stack).to(countResourcesLike('AWS::Lambda::Permission', 2, {
+      Action: 'lambda:InvokeFunction',
+      Principal: 'events.amazonaws.com'
+    }))
+
+    expectCdk(stack).to(countResourcesLike('AWS::Lambda::Permission', 1, {
+      Action: 'lambda:InvokeFunction',
+      Principal: 'sns.amazonaws.com'
+    }))
+  })
+
+  test('should create rule to trigger the lambdas ...', () => {
+    expectCdk(stack).to(countResources('AWS::Events::Rule', 1))
+
+    expectCdk(stack).to(haveResourceLike('AWS::Events::Rule', {
+      ScheduleExpression: 'cron(0 22 ? * MON-FRI *)',
+      State: 'ENABLED'
+    }))
+  })
+
+  test('should a role for each lambda', () => {
+    expectCdk(stack).to(countResources('AWS::IAM::Role', 3))
+  })
+
+  test('should subscribe a lambda to an sns topic', () => {
+    expectCdk(stack).to(countResources('AWS::SNS::Subscription', 1))
+
+    expectCdk(stack).to(haveResourceLike('AWS::SNS::Subscription', {
+      Protocol: 'lambda'
+    }))
+  })
+
+  test('should create policies to publish to sns ...', () => {
+    expectCdk(stack).to(countResourcesLike('AWS::IAM::Policy', 2, {
+      PolicyDocument: {
+        Statement: [{
+          Action: 'sns:Publish',
+          Effect: 'Allow',
+        }],
+        Version: '2012-10-17'
+      }
+    }))
+  })
+})
